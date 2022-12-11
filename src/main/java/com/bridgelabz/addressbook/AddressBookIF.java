7119c6344ca1455c721f151f7314a909c088da6f
Kunal <kunalsonawane730@gmail.com>
2022-12-12 02:07:03 +0530

    Add: read or write in address book by using File IO

--- src/main/java/com/bridgelabz/addressbook/AddressBookIF.java
+++ src/main/java/com/bridgelabz/addressbook/AddressBookIF.java
@@ -1,10 +1,14 @@
 package com.bridgelabz.addressbook;
 
+import java.util.List;
+
 public interface AddressBookIF {
 	void operation();
     void addContact();
     void deletePerson();
     void displayContents();
     void editPerson();
-	}
+    void writeToAddressBookFile();
+    List<String> readDataFromFile();
+}
 
